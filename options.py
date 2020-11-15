39464e5a384881de73bfbc77055a5874be4a7735
st2257st2257 <68289534+st2257st2257@users.noreply.github.com>
2020-11-15 13:33:41 -0500

    Update options.py

--- options.py
+++ options.py
@@ -1,10 +1,16 @@
 # base setting
 FPS = 5
+""" частота обновления экрана """
 
 screen_h_min = 0
 screen_h_max = 500
 screen_w_min = 0
 screen_w_max = 500
+""" настроцки положения экрана """
+
+board_x = 50
+board_y = 50
+""" координаты левого верхнего края доски """
 
 board_width = 400
 board_height = board_width
@@ -13,7 +19,6 @@ board_height = board_width
 game_mode = 0
 """ режим игры: 0 - два игрокаб 1 - один игрок """
 
-# color setting
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
@@ -22,6 +27,7 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+""" цвета для отображения на экране """
 
 CF_width = board_width / 8
 CF_height = board_width / 8
