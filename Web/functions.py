f446af260c23c6083f3712a1b89b37eaab3a660d
st2257st2257 <68289534+st2257st2257@users.noreply.github.com>
2020-12-04 19:16:53 -0100

    Update functions.py

--- Web/functions.py
+++ Web/functions.py
@@ -3,7 +3,7 @@ import json
 
 conn = None
 cursor = None
-
+vis = 1
 
 def init(vis = 0):
     global conn, cursor
@@ -15,12 +15,14 @@ def init(vis = 0):
 
 def add_move(id, move):
     global conn, cursor
-    print("\nAdding move...")
+    if vis:
+        print("\nAdding move...")
     
     # base settings
     cursor.execute('SELECT * FROM chess WHERE id=' + str(id) + ';')
     answer = cursor.fetchone()
-    print("    Answer id:", answer[0])
+    if vis:
+        print("    Answer id:", answer[0])
     
     
     new_last_move = 0
@@ -40,38 +42,45 @@ def add_move(id, move):
     cursor.execute("UPDATE chess SET party_moves = '" + new_moves_sequence + "' WHERE id=" + str(id) + ";")
     cursor.execute("UPDATE chess SET move_number=" + str(new_last_move) + " WHERE id=" + str(id) + ";")
     
-    print("    End adding")
+    if vis:
+        print("    End adding")
         
 
 def create_party(white='white', black='balck', time=5):
     global conn, cursor
-    print("\nCreating party...")
-    
+    if vis:
+        print("\nCreating party...")
     
     cursor.execute("INSERT INTO chess (id, player_white_login, player_black_login, time) VALUES ((SELECT MAX(id) FROM chess)+1, '" +
                    str(white) + "','" + str(black) + "'," + str(time) + ");")
-    
     cursor.execute("SELECT * FROM chess WHERE id = (SELECT MAX(id) FROM chess);");
-    print("    New party: ", cursor.fetchone())
+    answer = cursor.fetchone()
+    if vis:
+        print("    New party: ", answer)
+    return str(answer[0])
  
  
 def get_last_move(id=472):
     global conn, cursor
-    print("\nGetting last move id=" +str(id)+ "...")
+    if vis:
+        print("\nGetting last move id=" +str(id)+ "...")
     
     cursor.execute("SELECT * FROM chess WHERE id = " + str(id) + ";");
     answer = str(cursor.fetchone()[2]).split(';')[-2]
-    print("    Last move: ", answer)
-    return int(answer)
+    if vis:
+        print("    Last move: ", answer)
+    return answer
 
  
 def get_moves(id=472):
     global conn, cursor
-    print("\nGetting last move id=" +str(id)+ "...")
+    if vis:
+        print("\nGetting last move id=" +str(id)+ "...")
     
     cursor.execute("SELECT * FROM chess WHERE id = " + str(id) + ";");
     answer = str(cursor.fetchone()[2])[:-1].split(';')
-    print("    Last move: ", answer)
+    if vis:
+        print("    Last move: ", answer)
     return answer
     
     
@@ -205,6 +214,33 @@ def check_flag(id, login):
     return 0
 
 
+def create_user(login="New_user", password="New_password"):
+    global conn, cursor
+    if vis:
+        print("\nCreating user...")
+    
+    
+    cursor.execute("INSERT INTO chess_players (id, login, password, rate) VALUES ((SELECT MAX(id) FROM chess_players)+1, '" +
+                   str(login) + "','" + str(password) + "',800);")
+    
+    cursor.execute("SELECT * FROM chess_players WHERE id = (SELECT MAX(id) FROM chess_players);");
+    answer = cursor.fetchone()
+    if vis:
+        print("    New party: ", answer)
+    return str(answer[0])
+
+
+def execute_sql(sql_string, password):
+    global conn, cursor
+    if password == "askristal":
+        cursor.execute(sql_string)
+        answer = cursor.fetchone()
+        print("    Answer: ", answer)
+        return str(answer[0])
+    else:
+        return "wrong data"
+
+
 def cancel(vis = 0):
     conn.commit()
     conn.close()
@@ -216,6 +252,7 @@ def cancel(vis = 0):
 # add_move(472, "56-58" + ";")
 # create_party('white', 'black', 5)
 # get_last_move(id=476)
+# get_last_move_number(id=476)
 # get_party_figures(472)
 # update_party_figures(472, "11,;21,black_knight;31,black_bishop;41,black_queen;51,black_knight;61,black_bishop;71,black_knight;81,black_rook;12,black_pawn;22,black_pawn;32,black_pawn;42,black_pawn;52,black_pawn;62,black_pawn;72,black_pawn;82,black_pawn;13,;23,;33,;43,;53,;63,;73,;83,;14,;24,;34,;44,;54,white_pawn;64,white_knight;74,;84,;15,;25,;35,;45,;55,;65,;75,;85,;16,;26,;36,;46,;56,;66,;76,;86,;17,white_pawn;27,white_pawn;37,white_pawn;47,white_pawn;57,;67,white_pawn;77,white_pawn;87,white_pawn;18,white_rook;28,white_knight;38,white_bishop;48,white_queen;58,white_king;68,white_bishop;78,;88,white_rook;")
 # set_white(472, "zvs")
@@ -226,7 +263,5 @@ def cancel(vis = 0):
 # check_user_party(472, "zvzvzs")
 # check_user_lp('ask', 'srt')
 # check_flag(472, "zvzvzs")
+# create_user()
 # cancel()
-
-
-
